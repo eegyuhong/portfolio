6b973c6a9d8a8daeffd693f4721a3e524364c6ab
ee.gyuhong <ee.gyuhong@gmail.com>
2024-03-26 18:42:30 +0900

    CREATE footer 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Front from '@/components/front';
 import About from '@/components/about';
 import Works from '@/components/works';
 import Contact from '@/components/contact';
+import Footer from '@/components/footer';
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
         <hr />
         <Contact />
       </div>
+      <Footer />
     </main>
   );
 }
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="fixed text-sm text-right bottom-4 right-4 text-neutral-200">
+      <p>
+        Copyright 2024 Gyuhong Lee.
+        <br />
+        All rights reserved.
+      </p>
+    </footer>
+  );
+}
